9cf120e0696a5a061bcc0107edbd2abbc8c272e6
root <demefo@g.com>
2024-05-03 18:56:51 +0300

    heee

---
+++ DEVOPS_EASY_LEARNING/KUBERNETES/odilia/off.sh
@@ -0,0 +1,4 @@
+kubectl delete -f appserver.yml
+kubectl delete -f db.yml
+kubectl delete -f redis.yml
+kubectl delete -f ui.yml
---
+++ DEVOPS_EASY_LEARNING/KUBERNETES/odilia/on.sh
@@ -0,0 +1,4 @@
+kubectl apply -f db.yml
+kubectl apply -f appserver.yml
+kubectl apply -f redis.yml
+kubectl apply -f ui.yml
